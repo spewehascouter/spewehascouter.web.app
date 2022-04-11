fa2f529739e2ab13b4c30ed7ca1c8fc427f5300f
irawanria85 <102246214+infojandamuda@users.noreply.github.com>
2022-04-11 11:32:13 +0700

    speweha

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  printWidth: 120,
-  semi: false,
-  tabWidth: 2,
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'none'
-}
--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteUrl: 'https://spewehascouter.web.app',
-  generateRobotsTxt: true,
-  sitemapSize: 100,
-  exclude: ['']
-}
